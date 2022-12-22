917c18c17c6984bc8496a46b836c637cbf35a1e4
kumo829 <amonmar2000@hotmail.com>
2022-12-21 21:16:35 -0800

    ➕ADD test to validate the UsernameUniqueSpecification

---
+++ src/auth-service/src/test/java/com/javatutoriales/gaming/auth/domain/specifications/UsernameUniqueSpecificationTest.java
@@ -0,0 +1,47 @@
+package com.javatutoriales.gaming.auth.domain.specifications;
+
+import com.javatutoriales.gaming.auth.domain.entities.Member;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UsernameUniqueSpecificationTest {
+
+    private UsernameUniqueSpecification specification;
+
+    @Test
+    void givenASpecificationWithEmptyMemberList_whenTheValidationIsApplied_thenTheResultShouldBeTrue() {
+        Stream<Member> memberStream = Stream.empty();
+
+        specification = new UsernameUniqueSpecification(memberStream);
+        boolean memberExists = specification.isSatisfiedBy(Member.builder().email("member@email.com").build());
+        assertThat(memberExists).isFalse();
+    }
+
+    @Test
+    void givenASpecificationWithNonDuplicatedMemberList_whenTheValidationIsApplied_thenTheResultShouldBeFalse() {
+        Stream<Member> memberStream = Stream.of(
+                Member.builder().email("new.member@email.com").build(),
+                Member.builder().email("other.member@email.com").build(),
+                Member.builder().email("another.member@email.com").build());
+
+        specification = new UsernameUniqueSpecification(memberStream);
+        boolean memberExists = specification.isSatisfiedBy(Member.builder().email("member@email.com").build());
+        assertThat(memberExists).isFalse();
+    }
+
+    @Test
+    void givenASpecificationWithADuplicatedInTheMemberList_whenTheValidationIsApplied_thenTheResultShouldBeFalse() {
+        Stream<Member> memberStream = Stream.of(
+                Member.builder().email("member@email.com").build(),
+                Member.builder().email("other.member@email.com").build(),
+                Member.builder().email("another.member@email.com").build());
+
+        specification = new UsernameUniqueSpecification(memberStream);
+        boolean memberExists = specification.isSatisfiedBy(Member.builder().email("member@email.com").build());
+        assertThat(memberExists).isTrue();
+    }
+
+}
